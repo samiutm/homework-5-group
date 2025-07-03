0085a520c37230c0b0544f134aa662b2aca33ff4
AlbertoSaca <albsannar@gmail.com>
2025-07-03 19:19:46 -0400

    Finished:
    LogoutController.java
        TODO: Save the interactor in the instance variable.
        TODO: run the use case interactor for the
        logout use case
    
    LogoutOutputData.java
        TODO: save the parameters in the instance variables.

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,16 +1,19 @@
 package interface_adapter.logout;
 
 import use_case.logout.LogoutInputBoundary;
+import use_case.logout.LogoutInputData;
 
 /**
  * The controller for the Logout Use Case.
  */
 public class LogoutController {
 
-    private LogoutInputBoundary logoutUseCaseInteractor;
+    private final LogoutInputBoundary logoutUseCaseInteractor;
 
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
-        // TODO: Save the interactor in the instance variable.
+        // Save the interactor in the instance variable.
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
+
     }
 
     /**
@@ -18,8 +21,11 @@ public class LogoutController {
      * @param username the username of the user logging in
      */
     public void execute(String username) {
-        // TODO: run the use case interactor for the logout use case
-        // 1. instantiate the `LogoutInputData`, which should contain the username.
-        // 2. tell the Interactor to execute.
+        // run the use case interactor for the logout use case
+        // 1. Instantiate the `LogoutInputData`, which should contain the username
+        LogoutInputData inputData = new LogoutInputData(username);
+
+        // 2. Tell the interactor to execute the logout use case
+        logoutUseCaseInteractor.execute(inputData);
     }
 }
